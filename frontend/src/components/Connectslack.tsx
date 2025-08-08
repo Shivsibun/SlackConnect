7c364d8b9332721e6f7e7a34794e212032a6ac90
SHIV SUNDER PANDA <112812774+Shivsibun@users.noreply.github.com>
2025-08-09 00:50:12 +0530

    Update Connectslack.tsx

--- frontend/src/components/Connectslack.tsx
+++ frontend/src/components/Connectslack.tsx
@@ -1,14 +1,29 @@
-
 function Connectslack() {
   const SLACK_CLIENT_ID = import.meta.env.VITE_SLACK_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_SLACK_REDIRECT_URI;
 
   const scopes = ['channels:read', 'chat:write'].join(',');
 
-  const authUrl = "https://slack.com/oauth/v2/authorize?client_id=9309003919574.9318713485346&scope=channels:read,chat:write&redirect_uri=https%3A%2F%2Fpig-cool-jay.ngrok-free.app%2Fapi%2Fauth%2Fslack%2Fcallback";
+  const authUrl = `https://slack.com/oauth/v2/authorize?client_id=${encodeURIComponent(
+    SLACK_CLIENT_ID
+  )}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <a href={authUrl} style={{display: 'inline-block',marginTop: '30vh',padding: '40px 40px',backgroundColor: '#4A154B',color: 'white',borderRadius: '10px',textDecoration: 'none',fontWeight: 'bolder',fontSize: '30px',}} >
+      <a
+        href={authUrl}
+        style={{
+          display: 'inline-block',
+          marginTop: '30vh',
+          padding: '40px 40px',
+          backgroundColor: '#4A154B',
+          color: 'white',
+          borderRadius: '10px',
+          textDecoration: 'none',
+          fontWeight: 'bolder',
+          fontSize: '30px',
+        }}
+      >
         Connect to Slack
       </a>
     </div>
