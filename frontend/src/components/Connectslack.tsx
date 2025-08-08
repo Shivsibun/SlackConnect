69bdfbaaec3d3cdc4a25c3f7cbd5939b1d559b34
SHIV SUNDER PANDA <112812774+Shivsibun@users.noreply.github.com>
2025-08-09 00:24:18 +0530

    Update Connectslack.tsx

--- frontend/src/components/Connectslack.tsx
+++ frontend/src/components/Connectslack.tsx
@@ -5,10 +5,7 @@ function Connectslack() {
 
   const scopes = ['channels:read', 'chat:write'].join(',');
 
-  const authUrl = `https://slack.com/oauth/v2/authorize?client_id=${encodeURIComponent(
-    SLACK_CLIENT_ID
-  )}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-
+  const authUrl = "https://slack.com/oauth/v2/authorize?client_id=9309003919574.9318713485346&scope=channels:read,chat:write&redirect_uri=https%3A%2F%2Fpig-cool-jay.ngrok-free.app%2Fapi%2Fauth%2Fslack%2Fcallback";
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       <a href={authUrl} style={{display: 'inline-block',marginTop: '30vh',padding: '40px 40px',backgroundColor: '#4A154B',color: 'white',borderRadius: '10px',textDecoration: 'none',fontWeight: 'bolder',fontSize: '30px',}} >
